cc102a0bc0556a10a3a9e5cf27aed26d8dbfe65e
GheorgheDaniel <123gheorghedaniel@gmail.com>
2026-05-18 20:40:44 +0000

    merge fixed

--- Proiect_sortari/sortari/merge_sort.h
+++ Proiect_sortari/sortari/merge_sort.h
@@ -8,7 +8,7 @@
 
 #endif //PROIECT_SORTARI_MERGE_SORT_H
 inline void merge(int *dest, int* source1,const int size1,int* source2,const int size2) {
-    vector<int> temp(size1+size2);
+    std::vector<int> temp(size1+size2);
     int i=0,j=0;
     while (i<size1 && j<size2) {
         if (source1[i] <= source2[j]) {
