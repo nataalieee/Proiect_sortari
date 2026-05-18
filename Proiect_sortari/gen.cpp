ae8fe092cafb3cd14bbbc08593c25de9e7fda909
GheorgheDaniel <123gheorghedaniel@gmail.com>
2026-05-18 19:48:55 +0000

    generator, script, first testset added

---
+++ Proiect_sortari/gen.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+mt19937 get_rng() {
+	seed_seq seq {
+		(uint64_t) chrono::duration_cast<chrono::nanoseconds>(
+			chrono::high_resolution_clock::now().time_since_epoch()).count(),
+		(uint64_t) __builtin_ia32_rdtsc(),
+		(uint64_t) (uintptr_t) unique_ptr<char>(new char).get()
+	};
+	return mt19937(seq);
+}
+
+auto rng = get_rng();
+
+struct Operation {
+    int t, x;
+};
+
+
+vector <int> N_c_K(int N, int K) {
+    if(K > N || K < 0) {
+        return vector<int>();
+    }
+
+    vector <int> ret(N);
+    iota(ret.begin(), ret.end(), 0);
+    shuffle(ret.begin(), ret.end(), rng);
+    ret.resize(K);
+    sort(ret.begin(), ret.end());
+
+    return ret;
+}
+
+vector <int> S_as_N(int S, int N) {
+    vector <int> ret;
+    if(N == 0) {
+        return vector<int>();
+    }
+    vector <int> c = N_c_K(N + S - 1, N - 1);
+    c.insert(c.begin(), -1);
+    c.push_back(N + S - 1);
+    
+    
+    for(int i = 1; i < c.size(); i++) {
+        ret.push_back(c[i] - c[i - 1] - 1);
+    }
+    return ret;
+}
+
+int prim(int x) {
+    if(x == 1 || x == 0) return false;
+    for(int i = 2; i * i <= x; i++) {
+        if(x % i == 0) return false;
+        
+    }
+    return true;
+}
+
+
+int32_t main(int32_t argc, char *argv[]) {
+    // cin.tie(0); cout.tie(0);
+    // ios_base::sync_with_stdio(false);
+    int N = atoi(argv[1]);
+    int seed = atoi(argv[2]);
+    uniform_int_distribution<int>A(1, (1LL << 31) - 1);
+    cout << N << "\n";
+    vector<int>B(N);
+    for(int i = 0; i < N; i++) {
+        B[i] = A(rng);
+    }
+    
+    // if(seed == 1) {
+    //     for(int i = 0; i < N; i++) {
+    //         B[i] %= 100;
+    //     }
+    // }
+    if(seed == 2) {
+        sort(B.begin(), B.end());
+        reverse(B.begin(), B.end());
+    }
+    if(seed == 3) {
+        int bucket_size = 400;
+        for(int i = 0; i < B.size(); i += bucket_size) {
+            int le = i;
+            int ri = min(i + bucket_size - 1, (int) B.size() - 1);
+               if(le > ri) continue;
+            sort(B.begin() + le, B.begin() + ri);
+            reverse(B.begin() + le, B.begin() + ri);
+        }
+    }
+
+    for(auto i : B) cout << i << ' ';
+}   
+
+/*
+
+
+
+*/
--- Proiect_sortari/main.cpp
+++ Proiect_sortari/main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
-#include "sortari/quick_sort.h"
+#include <algorithm>
+#include <assert.h>
+#include "sortari/radix_sort.h"
 int main() {
-    std::vector<int> v={2,4,1,2,5,3,2};
+    std::cin.tie(0); std::cout.tie(0);
+    std::ios_base::sync_with_stdio(false);
+    int N; std::cin >> N;
+    std::vector<int> v(N);
+    for(auto &x : v) std::cin >> x;
     customsort(v);
-    for (auto i : v) {std::cout<<i<<" ";}
+    assert(std::ranges::is_sorted(v));
     return 0;
 }
+
+/*
+    radix nu sorteaza
+    merge ia segfault
+    heap nu sorteaza
+
+
+
+*/
